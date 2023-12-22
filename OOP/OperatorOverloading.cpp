1a62fe202b2578093a322e5f1ecd7c625b1c70fb
tejaspatil1936 <tejaspatil1936@gmail.com>
2023-12-22 22:25:30 +0530

    operator overloading basic code

---
+++ OOP/OperatorOverloading.cpp
@@ -0,0 +1,32 @@
+// Unary Operator overloading
+#include <iostream>
+using namespace std;
+class Demo
+{
+private:
+    int a, b;
+
+public:
+    Demo(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+    void show()
+    {
+        cout << "A = " << a << " " << "B = " << b << endl;
+    }
+    void operator-()
+    {
+        a = -a;
+        b = -b;
+    }
+};
+int main()
+{
+    Demo d(-10, 20);
+    d.show();
+    -d;
+    d.show();
+    return 0;
+}
