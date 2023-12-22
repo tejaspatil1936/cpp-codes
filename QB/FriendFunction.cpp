2615d250ad257f947f41577aa15264c16c0f8e05
tejaspatil1936 <tejaspatil1936@gmail.com>
2023-12-22 22:59:38 +0530

    question bank

---
+++ QB/FriendFunction.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Square
+{
+private:
+    int side;
+
+public:
+    // Default constructor
+    Square()
+    {
+        side = 10; // default side length
+    }
+
+    // Friend function declaration
+    friend int printSideLength(Square);
+};
+
+// Friend function definition
+int printSideLength(Square s)
+{
+    s.side += 10;  // Increasing the side length by 10
+    return s.side; // Returning the new side length
+}
+
+int main()
+{
+    Square s;
+
+    cout << "Side length of Square: " << printSideLength(s) << endl;
+
+    return 0;
+}
