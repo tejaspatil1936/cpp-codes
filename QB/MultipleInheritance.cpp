74a02a76787a316bb521bb39cd7c597095a7504e
tejaspatil1936 <tejaspatil1936@gmail.com>
2023-12-22 23:48:56 +0530

    question bank

---
+++ QB/MultipleInheritance.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ClassA {
+public:
+    void displayA() {
+        cout << "Class A" << endl;
+    }
+};
+
+class ClassB {
+public:
+    void displayB() {
+        cout << "Class B" << endl;
+    }
+};
+
+class ClassC : public ClassA, public ClassB {
+public:
+    void displayC() {
+        cout << "Class C" << endl;
+    }
+};
+
+int main() {
+    ClassC obj;
+
+    // Calling methods from both base classes
+    obj.displayA();
+    obj.displayB();
+
+    // Calling method of derived class
+    obj.displayC();
+
+    return 0;
+}
