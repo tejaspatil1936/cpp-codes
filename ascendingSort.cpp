64702e61e18db115e4597ea3cd511e032daa4e74
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-03 12:00:00 +0530

    dsa  practice

---
+++ ascendingSort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the no of elements in array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        int mini = i;
+        for (int j = 0; j < count; j++)
+        {
+            /* code */
+        }
+        
+    }
+    
+    return 0;
+}
