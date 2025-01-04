c720cef9b4cd1503b80db523cc1f01dee75c496d
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-04 12:00:00 +0530

    removed unnecessary code

--- explicit.cpp
+++ explicit.cpp
@@ -1,25 +1,11 @@
 #include <iostream>
 using namespace std;
-// int main()
-// {
-    //     double x=1.2;
 
-    //     int sum = (int)x + 1;
-    //     cout<<"Sum= "<<sum;
-    //     return 0;.
-    // }
     int main()
     {
         int a = 45;
         float b = 45.54;
-        /*
 
-        cout<<"The value of a is : "<<(float)a<<endl;
-        cout<<"The value of a is : "<<float(a)<<endl;
-
-        cout<<"The value of a is : "<<(int)b<<endl;
-        cout<<"The value of a is : "<<int(b)<<endl;
-        */
 
         int c = int(b);
         cout << "The value of c is : " << c << endl;
