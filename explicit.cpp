0f55695b9cb85c525558fc10eb2133c355ca41fa
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-06 12:00:00 +0530

    removed unnecessary code

--- explicit.cpp
+++ explicit.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
 
-    int main()
-    {
-        int a = 45;
-        float b = 45.54;
+int main()
+{
+    int a = 45;
+    float b = 45.54;
 
+    int c = int(b);
+    cout << "The value of c is : " << c << endl;
+    cout << "the expression is : " << a + b << endl;
+    cout << "the expression is : " << a + int(b) << endl;
+    cout << "the expression is : " << a + (int)b << endl;
 
-        int c = int(b);
-        cout << "The value of c is : " << c << endl;
-        cout << "the expression is : " << a + b << endl;
-        cout << "the expression is : " << a + int(b) << endl;
-        cout << "the expression is : " << a + (int)b << endl;
-
-        return 0;
-    }
+    return 0;
+}
