6a0a41b0f2b06dc8ca178ab7d818bc6c35130b25
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-12 10:00:00 +0530

    cpp practice

--- reversealpharamp copy.cpp
+++ reversealpharamp copy.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-void pattern15(int N)
-{
-    for (int i = 0; i < N; i++)
-    {
+void pattern15(int N){
+    for (int i = 0; i < N; i++){
         for (char ch = 'A'; ch <= 'A' + (N - i - 1); ch++)
-        {
             cout << ch << " ";
         }
         cout << endl;
