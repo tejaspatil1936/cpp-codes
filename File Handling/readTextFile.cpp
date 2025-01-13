67b78062bce2168f98b59766d0e6613e009bad66
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-13 10:00:00 +0530

    cpp practice

---
+++ File Handling/readTextFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream file("example.txt");
+    string line;
+    if(file.is_open()) {
+        while(getline(file, line)) {
+            cout << line << endl;
+        }
+        file.close();
+    } else {
+        cerr << "Unable to open file!";
+    }
+    return 0;
+}
