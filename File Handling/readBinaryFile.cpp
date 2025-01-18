492cf41796498c9c8358ea070733c4258f55ec75
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-18 10:00:00 +0530

    cpp practice

---
+++ File Handling/readBinaryFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+};
+
+int main() {
+    ifstream file("data.bin", ios::binary);
+    Person p;
+    if(file.read(reinterpret_cast<char*>(&p), sizeof(p))) {
+        cout << "Name: " << p.name << "\nAge: " << p.age;
+    }
+    file.close();
+    return 0;
+}
