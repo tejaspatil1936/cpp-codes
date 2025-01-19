67fdf0300829bfc59a7c4829d7cb3457e5ecd14e
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-19 10:00:00 +0530

    cpp practice

---
+++ File Handling/getFileSize.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file("example.txt", ios::ate | ios::binary);
+    if(file.is_open()) {
+        cout << "File size: " << file.tellg() << " bytes";
+        file.close();
+    }
+    return 0;
+}
