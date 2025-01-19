5d291b167b2941574dd290be5926706d07407854
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-19 10:00:00 +0530

    cpp practice

---
+++ File Handling/copyFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream src("source.txt", ios::binary);
+    ofstream dest("dest.txt", ios::binary);
+    dest << src.rdbuf();
+    cout << "File copied!";
+    return 0;
+}
