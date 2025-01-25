d4dc4e80243c7039d3e9447d7ba8e04074390cf6
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-25 10:00:00 +0530

    cpp practice

--- File Handling/countLines.cpp
+++ File Handling/countLines.cpp
@@ -2,11 +2,13 @@
 #include <fstream>
 using namespace std;
 
-int main() {
+int main()
+{
     ifstream file("example.txt");
     int count = 0;
     string line;
-    while(getline(file, line)) count++;
+    while (getline(file, line))
+        count++;
     cout << "Total lines: " << count;
     return 0;
 }
