2defc7fe86c3c0e0753d49de09984b92df8d317e
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-01-26 23:57:40 +0530

    bubble sort

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "enter the no of elements in array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+    cout << "sorted array : ";
+    printArray(arr, n);
+    return 0;
+}
