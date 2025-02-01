81222d350da2fdf7b9cd6fc656e38d1f947e9f40
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-01 14:45:40 +0530

    exploring cpp

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+/* 
+to view the assembly code after compilation of this code run this command 
+on ubuntu: g++ -S -masm=intel -o hello.s hello.cpp
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
