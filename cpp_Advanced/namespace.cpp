783eeff98f13128e40425f79ee2ab696951bee8f
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-02-15 23:28:01 +0530

    codes

---
+++ cpp_Advanced/namespace.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <typeinfo>
+#include <vector>  
+#include <map>     
+
+int main() {
+    std::cout << "Type of std::cout: " << typeid(std::cout).name() << std::endl;
+    std::cout << "Type of std::cin: " << typeid(std::cin).name() << std::endl;
+    std::cout << "Type of std::string: " << typeid(std::string).name() << std::endl;
+    std::cout << "Type of std::vector<int>: " << typeid(std::vector<int>()).name() << std::endl;
+    std::cout << "Type of std::map<int, int>: " << typeid(std::map<int, int>()).name() << std::endl;
+    return 0;
+}
