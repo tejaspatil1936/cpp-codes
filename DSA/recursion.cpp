47dce700f45629e1aad0472230ec7839172bfb52
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-04 12:00:00 +0530

    codes

--- DSA/recursion.cpp
+++ DSA/recursion.cpp
@@ -1,8 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-// for this recursion we will get
-// Segmentation fault (core dumped)
-// because there is no termination condition memory stack will get overflowed
 void print()
 {
     cout << "Hello, World !" << endl;
@@ -13,4 +10,3 @@ int main()
     print();
     return 0;
 }
-// refer recursion1.cpp for correct code
