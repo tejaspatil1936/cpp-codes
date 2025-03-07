082105066d3e5f8db360aab8472edd34200b71a2
tejaspatil1936 <tejaspatil1936@gmail.com>
2025-03-07 12:00:00 +0530

    codes

--- DSAcollege/array_traverse.cpp
+++ DSAcollege/array_traverse.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main()
 {
     int linearAarray[5] = {1, 2, 3, 4, 5};
-    
+
     // step 1
     int k = 0;
 
     // step 2
-    while (k <= 4) 
+    while (k <= 4)
     {
-        //step 3
+        // step 3
         linearAarray[k]++;
 
-        //step 4
+        // step 4
         k = k + 1;
     }
     // end of step 2
